ed96891b2d66ac2c0cc9097855de8857f6a24023
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2019-09-08 17:40:17 +0200

    Initial commit #2

---
+++ lib/hashire-game.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+import 'package:flutter/gestures.dart';
+
+class Hashire extends Game {
+  Size screenSize;
+  bool hasWon = false;
+
+  void render(Canvas canvas) {
+    //background
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff373f43);
+    canvas.drawRect(bgRect, bgPaint);
+
+    //white box
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect =
+        Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
+    Paint boxPaint = Paint();
+
+    if (hasWon) {
+      boxPaint.color = Color(0xff00ff00);
+    } else {
+      boxPaint.color = Color(0xffffffff);
+    }
+
+    canvas.drawRect(boxRect, boxPaint);
+  }
+
+  void update(double t) {
+    // TODO
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
+  }
+
+  void onTapDown(TapDownDetails d) {
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    if (d.globalPosition.dx >= screenCenterX - 75
+        && d.globalPosition.dx <= screenCenterX + 75
+        && d.globalPosition.dy >= screenCenterY - 75
+        && d.globalPosition.dy <= screenCenterY + 75
+    ) {
+      hasWon = !hasWon;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:hashire/hashire-game.dart';
+import 'package:flutter/gestures.dart';
 
-void main() {}
+void main() async{
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.landscapeLeft);
+
+  Hashire game  = Hashire();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper);
+}
